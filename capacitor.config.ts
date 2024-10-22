39814c4cb12b7c7371fde3a5d1e16daa9bc45c47
Goutham-Harshith <54266143+Goutham-Harshith@users.noreply.github.com>
2024-10-22 18:50:48 +0530

    ios related changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cloud9.creatives',
   appName: 'Cloud9 Creatives',
   webDir: 'www'
 };
